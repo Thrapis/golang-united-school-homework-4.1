f9b9ba424aeea631ac4d3e1edfc65ac191c9449c
Artyom Belski <artbelski@gmail.com>
2022-05-12 18:19:33 +0300

    First try

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	var buf = []rune(input)
+	for i := 0; i < len(buf)-1-i; i++ {
+		buf[i], buf[len(buf)-1-i] = buf[len(buf)-1-i], buf[i]
+	}
+	output = string(buf)
 	return output
 }
